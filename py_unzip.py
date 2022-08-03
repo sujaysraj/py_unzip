c08722fb3e3bd22ca5a0e48a8873c8bee8bcad61
Sujay Raj <sraj3@atlassian.com>
2022-08-03 13:00:32 +0530

    added checks for the directory created

--- py_unzip.py
+++ py_unzip.py
@@ -4,6 +4,13 @@ import os
 import sys
 import zipfile,fnmatch,glob
 
+def listToString(extn): 
+    
+    # initialize an empty string
+    str1 = "" 
+    
+    # return string  
+    return (str1.join(extn))
 
 #Set Current direct as root
 
@@ -14,10 +21,13 @@ rootPath = r"."
 pattern = '*.zip'
 for root, dirs, files in os.walk(rootPath):
     for filename in fnmatch.filter(files, pattern):
-        print(os.path.join(root, filename))
+        print("Currently extracting:", os.path.join(root, filename))
         extn = filename.split(".")
+        extn_1 = listToString(extn[0])
+        extn_type = extn_1 + "_zip"
         # Check if the filename of the zip file exists in the list of directories. 
-        if (extn[0] not in dirs):
-            zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
-
+        if (extn_type not in dirs):
+            zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, extn_type))
+        else:
+            print("\nExtraction failed. Error: Directory",extn_type,"already exists.")
 
